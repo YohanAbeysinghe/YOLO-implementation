06b3264d99b2ace5a135e85253e020183da30181
YohanAbeysinghe <abeysingheyohan@gmail.com>
2024-02-04 10:53:27 +0530

    initialize

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # YOLO-implementation
 
 Command to run YOLOv8 using the webcam input - *yolo predict model=yolov8n.pt source=0 show=True*
+ 
---
+++ model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+architecture_config = [
+    # (Kernel_size, Channel_count, Padding, Stride)
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    # (Conv1, Conv2, Numberofrepeats)
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channes, **kwargs):
+        super(CNNBlock, self).__init__()
+        
